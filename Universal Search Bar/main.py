4237cf9d5609161949b6d9f32e0262c1fc6697af
Ajay Kumar Singh <ajaykumar.kumar900@gmail.com>
2018-03-17 12:51:35 +0530

    Google Speech Recognition API Integrated
    Additional feature in search bar

--- Universal Search Bar/main.py
+++ Universal Search Bar/main.py
@@ -9,6 +9,7 @@ root = ttk.Tk()
 # Main Window
 root.title('My Search Engine')
 # root.iconbitmap()
+# root.configure(background='white')
 root.geometry("470x50")
 label1 = ttk.Label(root, text='Query')
 label1.grid(row=0, column=0)
@@ -16,20 +17,22 @@ entry1 = ttk.Entry(root, width=40)
 entry1.grid(row=0, column=1)
 
 btn2 = StringVar()
-'''
-def buttonClick():
-    mixer.init()
-    mixer.music.load('chime1.mp3')
-    r = sr.Recognizer()
-    r.pause_threshold = 0.7
-    r.energy_threshold = 400
 
-    with sr.Microphone(as source):
-        try:
-            audio = r.listen(source, timeout=5)
-            message = str(r.recognize_google(audio, key=))
-    pass
-'''
+
+def buttonClick():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    try:
+        message = str(r.recognize_google(audio))
+        entry1.focus()
+        entry1.delete(0, END)
+        entry1.insert(0, message)
+    except sr.UnknownValueError:
+        print('Error')
+    except sr.RequestError as e:
+        print('Internet not working')
+    callback()
 
 
 # Callback() function is passed to the button and gives result in Google results.
@@ -39,7 +42,6 @@ def callback():
         webbrowser.open('https://google.com/search?q=' + entry1.get())
     elif btn2.get() == 'DuckDuckGo':
         webbrowser.open('https://duckduckgo.com/?q=' + entry1.get())
-
     elif btn2.get() == 'Amazon':
         webbrowser.open('https://www.amazon.in/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' + entry1.get())
     elif btn2.get() == 'Youtube':
@@ -59,7 +61,7 @@ def get(event):
 
 
 # Search button
-button1 = ttk.Button(master=root, text='Search', width=10, command=callback, relief=SOLID, borderwidth=2)
+button1 = ttk.Button(master=root, text='Search', width=10, command=callback, relief=SOLID)
 button1.grid(row=0, column=2)
 entry1.bind('<Return>', get)
 
@@ -72,6 +74,10 @@ MyButton4 = ttk.Radiobutton(master=root, text='Amazon', value='Amazon', variable
 MyButton4.grid(row=1, column=2, columnspan=1)
 MyButton5 = ttk.Radiobutton(master=root, text='Youtube', value='Youtube', variable=btn2, width=10)
 MyButton5.grid(row=1, column=3, columnspan=1)
+
+# MICROPHONE SETTINGS
+mic = ttk.Button(root, text='MIC', command=buttonClick, relief=FLAT)
+mic.grid(row=0, column=3)
 
 entry1.focus()
 ttk.mainloop(0)
